88f14e8975c1f1d01b7613559174c69c73991da4
Yigezu Mulugeta <Yigezumb@gmail.com>
2020-09-20 16:44:18 -0400

    two functions that update ui and event handler for the list of cities added

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -42,6 +42,7 @@ $("#search").on("click", function () {
       lat = response.city.coord.lat;
       lon = response.city.coord.lon;
       getSetCityFromLocalStorage(inputValue);
+      updateWeatherUi(response);
     });
     //   var queryUrl2 = "http://api.openweathermap.org/data/2.5/onecall?lat="+ lat + "&lon="+lon+"exclude=hourly,minutely&appid=7118d3eda51d9d8e760f326175a3947b"
     //     $.ajax({
@@ -54,8 +55,29 @@ $("#search").on("click", function () {
 });
 
 function updateListOfCitiesUi() {
-  for (var i = 0; i < b.length; i++) {
-    var pEl = $("<p class='cities'>").text(b[i]);
+  var citiesLocal = JSON.parse(localStorage.getItem("citiesserched"));
+  var citiesProp = citiesLocal.cities;
+  for (var i = 0; i < citiesProp.length; i++) {
+    var pEl = $("<p class='cities border'>").text(citiesProp[i]);
     $("#listOfCitiesSearched").append(pEl);
   }
 }
+
+function updateWeatherUi(wInput) {
+  console.log(wInput);
+}
+// click event handler that responds on user click on the list of cities. When one of the city
+//  is click the weather information for that city will be displayed.
+$("#listOfCitiesSearched").on("click", $(".cities"), function () {
+  var inputValue = $(this).text();
+  var queryUrl =
+    "http://api.openweathermap.org/data/2.5/forecast?q=" +
+    inputValue +
+    "&cnt=1&appid=7118d3eda51d9d8e760f326175a3947b";
+  $.ajax({
+    url: queryUrl,
+    method: "GET",
+  }).then(function (response) {
+    updateWeatherUi(response);
+  });
+});
